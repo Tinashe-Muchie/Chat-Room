1f60f884c1fd429380355cb7430c55f48228963c
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-02-25 17:37:59 +0200

    displaying chats

--- main/client/src/Components/Chat-Area/Tabs/Chats.js
+++ main/client/src/Components/Chat-Area/Tabs/Chats.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {ListGroup} from 'react-bootstrap'
+import {Context} from '../Context/GlobalContext'
 
 function Chats() {
+
+    const {Chats} = useContext(Context)
+
     return (
         <div>
-            ChatArea
+             <ListGroup variant="flush">
+                {Chats.map((chat)=>(
+                    <ListGroup.Item 
+                        key={0} 
+                        action
+                        active
+                        onClick
+                        className="list-item-wrapper text-left"
+                    >
+                            {chat.username.join(',')}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </div>
     )
 }
