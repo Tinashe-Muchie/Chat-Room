88777c9dde4361f36d0ae7a3c04a85c49c7ea808
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-02-27 20:20:24 +0200

    sidebar modifcation

--- main/client/src/Components/Chat-Area/SideBar.js
+++ main/client/src/Components/Chat-Area/SideBar.js
@@ -1,36 +1,30 @@
 import React, {useState} from 'react'
-import {Tab, Nav, Button, Modal} from 'react-bootstrap'
+import {Tab, Nav, Button, Modal, Navbar} from 'react-bootstrap'
 import Chats from './Tabs/Chats'
 import Contacts from './Tabs/Contacts'
-import History from './Tabs/History'
 import ContactsModal from './Modals/ContactsModal'
-import ChatsModal from './Modals/ContactsModal'
-import HistoryyModal from './Modals/HistoryyModal'
+import ChatsModal from './Modals/ChatsModal'
 
 function SideBar() {
     const Chats_Key = 'chats';
     const Contacts_Key = 'contacts'
-    const History_Key = 'history'
 
     const [key, setKey] = useState(Chats_Key)
     const [show, setShow] =useState(false)
 
-    const chatOpen = key === Chats_Key
-    const contactsOpen = key === Contacts_Key
+    const chatsOpen = key === Chats_Key
+    const handleOpen = ()=> setShow(true)
     const handleClose = ()=> setShow(false)
     
     return (
         <div style={{width:'20vw'}} className="d-flex flex-column">
             <Tab.Container activeKey= {key} onSelect= {(k)=>setKey(k)}>
-                <Nav variant="tabs" className="justify-content-center">
+                <Nav justify variant="pills" className="my-1">
                     <Nav.Item>
                         <Nav.Link eventKey={Chats_Key}>Chats</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link eventKey={Contacts_Key}>Contacts</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey={History_Key}>History</Nav.Link>
                     </Nav.Item>
                 </Nav>
                 <Tab.Content className="tab-content overflow-auto flex-grow-1">
@@ -40,26 +34,23 @@ function SideBar() {
                     <Tab.Pane eventKey={Contacts_Key}>
                         <Contacts />
                     </Tab.Pane>
-                    <Tab.Pane eventKey={History_Key}>
-                        <History />
-                    </Tab.Pane>
                 </Tab.Content>
                 <div className="id-tab mt-1 text-small text-muted">
                     Your ID: <span></span>
                 </div>
-                <Button className="my-1" onClick={()=>setShow(true)} variant="outline-primary">
+                <Button className="mt-1 mb-3" onClick={handleOpen} variant="outline-primary">
                     {
-                          (chatOpen) ?  'New Chat'
-                        : (contactsOpen) ? 'New Contact'
-                        : 'History'
+                        (chatsOpen) 
+                        ?  'New Chat'
+                        :  'New Contact'
                     }
                 </Button>
                 </Tab.Container>
                 <Modal show={show} onHide={handleClose}>
                     {
-                          (chatOpen) ? <ChatsModal handleClose={handleClose} />
-                        : (contactsOpen) ? <ContactsModal handleClose={handleClose}/>
-                        : <HistoryyModal />
+                        (chatsOpen) 
+                        ? <ChatsModal handleClose={handleClose} />
+                        : <ContactsModal handleClose={handleClose} />
                     }
                 </Modal>
         </div>
