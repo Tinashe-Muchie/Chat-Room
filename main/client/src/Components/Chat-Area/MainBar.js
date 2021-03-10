4d413972cf796dcab77b29b7f31a8357a2a85f53
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-10 19:15:28 +0200

    mainbar modifications - messages

--- main/client/src/Components/Chat-Area/MainBar.js
+++ main/client/src/Components/Chat-Area/MainBar.js
@@ -2,22 +2,47 @@ import React, {useState, useContext} from 'react'
 import { Form, InputGroup, Button} from 'react-bootstrap'
 import { Context } from './Context/GlobalContext'
 
+function equalityArray(a, b){
+    if(a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+    
+    return a.every((item, index)=>{
+        return item === b[index]
+    })
+}
+
 function MainBar() {
 
-    const {createMessage, selectedChatIndex} = useContext(Context)
+    const {createMessage, selectedChatIndex, Message} = useContext(Context)
     const [text, setText] = useState('')
 
     function handleSubmit(e){
         e.preventDefault()
-
+        sendMessage()
         setText('')
     }
 
-    const message = selectedChatIndex.map((chat)=>{
-        const contactId = chat.Chat.map(cha=>cha.id)
-        return {id: contactId, message: text}
-    })
-    console.log(message)
+    const recipient = selectedChatIndex.map((chat)=>(chat.id))
+    const msgs = Message.map((msg)=>msg.recipients).find((recipient)=>{return recipient===selectedChatIndex})
+    console.log(msgs)
+
+    function addMessageToChat(recipientIds, text, sender){
+        const newMessage = {sender, text}
+        const messageExists = Message.map((msg)=>msg.recipients).includes(recipientIds)
+
+        if(messageExists){
+            const msgs = Message.map((msg)=>msg.recipients).find((recipient)=>{return recipient===recipientIds})
+        } else {
+            const message = {recipients: recipientIds, messages: [newMessage]}
+            createMessage(message)
+        }    
+    }
+
+    function sendMessage(){
+        addMessageToChat(selectedChatIndex, text)
+    }
 
     return (
         <div className="d-flex flex-column flex-grow-1 ml-1">
