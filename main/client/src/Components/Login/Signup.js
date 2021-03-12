534a03b2fd90298bba85ef7a98ffeb16b478bf83
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-12 20:09:29 +0200

    login  modifications

--- main/client/src/Components/Login/Signup.js
+++ main/client/src/Components/Login/Signup.js
@@ -1,69 +1,37 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React, {useRef} from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
-function Signup() {
+function Signup({onIdSubmit}) {
     
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    
-    function Signup(e) {
-        e.preventDefault();
+    const idRef = useRef()
 
-        axios.post('http://localhost:9000/users/signup', {
-            username: username,
-            password: password,
-        })
-            .then((res)=>{
-                console.log(res)
-            })
-            .catch((error)=>{
-                console.error(error)
-            })
-        
-        setUsername('');
-        setPassword('');
-    }
-
-    function Login(e) {
+    function handleSubmit(e){
         e.preventDefault()
 
-        axios.post('http://localhost:9000/users/login', {
-            username: username,
-            password: password,
-        })
-            .then(res=>{console.log(res)})
-            .catch(error=>{
-                console.error(error)
-            })
+        onIdSubmit(idRef.current.value)
+        idRef.current.value = null
+    }
+    
+    function generateID(){
+        onIdSubmit(uuidv4())
     }
 
     return (
         <div>
             <div className="container">
-                <form className="form-wrapper">
-                <div className="form-group mb-3">
+                <form onSubmit={handleSubmit} className="form-wrapper">
+                <div className="form-group mb-4">
                     <input 
                         type="text" 
                         id="username"
-                        placeholder="&#61447; username"
-                        value= {username}
-                        onChange = {(e)=>{setUsername(e.target.value)}}
-                        className="form-control-lg"
-                    />
-                </div>
-                <div className="form-group mb-5">
-                    <input 
-                        type="password"
-                        id="password"
-                        placeholder="&#xf023; password"
-                        value= {password}
-                        onChange = {(e)=>{setPassword(e.target.value)}}
+                        ref = {idRef}
+                        placeholder="Enter your ID"
                         className="form-control-lg"
                     />
                 </div>
                 <div>
-                    <button type="submit" className="btn btn-outline-primary mx-2" onClick={Login}>Login</button>
-                    <button type="submit" className="btn btn-outline-primary mx-2" onClick={Signup}>Signup</button>
+                    <button type="submit" className="btn btn-outline-primary mx-2">Login</button>
+                    <button type="button" className="btn btn-primary mx-2" onClick={generateID}>Autogenerate Id</button>
                 </div>   
                 </form>
             </div>   
