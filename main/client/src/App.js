d231b30146d7077a67332fba693d9a3bedbcbba3
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-12 20:08:30 +0200

    app  modifications- id

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GlobalContext from './Components/Chat-Area/Context/GlobalContext'
 import './App.css';
-//import Signup from './Components/Login/Signup'
+import Signup from './Components/Login/Signup'
 import ChatArea from './Components/Chat-Area/ChatArea'
 
 function App() {
 
+  const [id, setId] = useState()
+
   return (
     <div className="App">
       <GlobalContext>
-        <ChatArea />
-        {/*<Signup />*/}
+        { id
+          ? <ChatArea id={id} />
+          : <Signup onIdSubmit={setId} />
+        } 
       </GlobalContext>   
     </div>
   );
